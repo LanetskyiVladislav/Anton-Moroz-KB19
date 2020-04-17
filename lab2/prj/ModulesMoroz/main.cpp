adbaf5634a9151453277462612488d70efa62e1c
Anton Moroz <moroz.t27@gmail.com>
2020-04-17 16:37:38 +0300

    закінчив з функцією s_calculation() та бібліотекою

--- lab2/prj/ModulesMoroz/main.cpp
+++ lab2/prj/ModulesMoroz/main.cpp
@@ -101,7 +101,60 @@ static void q_calculation(string x1, string y1, string z1){
     cout<<"Q = "<<abs( sin( abs( y - pow(z, x) ) ) + sqrt(x) - sqrt( 0.5 * pow( y*z, 4 ) + 2 * M_PI) )<<endl;
 }
 
-static void s_calculation(){
+static void s_calculation(string x1, string y1, string z1){
+    bool rx = 1, ry = 1, rz = 1;
 
+//перевірка формата введених значень
+    try{
+        check_l(x1);
+    }catch(exception e){
+        rx=0;
+    }
+    try{
+        check_l(y1);
+    }catch(exception e){
+        ry=0;
+    }
+    try{
+        check_l(z1);
+    }catch(exception e){
+        rz=0;
+    }
+    showmess(rx, ry, rz);
+
+//перевірка можливості перевести значення рядкових змінних у числові
+    float x, y, z;
+    try{
+        x=stof(x1);
+    }catch(exception e){
+        rx=0;
+    }
+    try{
+        y=stof(y1);
+    }catch(exception e){
+        ry=0;
+    }
+    try{
+        z=stof(z1);
+    }catch(exception e){
+        rz=0;
+    }
+    showmess(rx, ry, rz);
+    ry=1;
+    rz=1;
+// валідатор
+    //x
+    if( x < ( (-7) * M_PI )) {
+        rx = 0;
+    }
+    showmess(rx, ry, rz);
+
+    //система рівностей
+    if( (( pow(M_E, abs(x)) + ( sqrt( abs( y-z ) ) )/( sin(z*y) ) ) <0)||( pow(M_E, abs(x)) < ( sqrt( abs( y-z ) ) )/( sin(z*y) ) )){
+        cout<<"Значення змінних не задовольняє вираз!"<<endl;
+    }
+
+// обчислення
+    cout<<"S = "<< 0.5*((y*y+2*z)/(sqrt(7*M_PI+x)))-sqrt(pow(M_E, abs(x)) + ( sqrt( abs( y-z ) ) )/( sin(z*y) ))<<endl;
 }
 
